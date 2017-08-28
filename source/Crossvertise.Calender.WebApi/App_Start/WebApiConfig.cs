1bdcc53924b35df72fc0a70786b984c40657ee73
kmansou <karim.mansour91@gmail.com>
2017-08-28 02:48:11 +0200

    Add Event api controller remove Values api controller

--- source/Crossvertise.Calender.WebApi/App_Start/WebApiConfig.cs
+++ source/Crossvertise.Calender.WebApi/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace Crossvertise.Calender.WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "ActionBased",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
---
+++ source/Crossvertise.Calender.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,48 @@
+﻿using Crossvertise.Calender.BusinessServices.Core.Services;
+using Crossvertise.Calender.BusinessServices.Implementation;
+using Crossvertise.Calender.DAL.EF.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Crossvertise.Calender.BusinessServices.Core.Models;
+
+namespace Crossvertise.Calender.WebApi.Controllers
+{
+    public class EventsController : ApiController
+    {
+        private readonly IEventServices _eventServices;
+
+        public EventsController()
+        {
+            _eventServices = new EventServices(new EFxCalenderUoW());
+        }
+
+        // GET api/Events/GetMonthEvents?monthId={monthId}
+        public HttpResponseMessage GetMonthEvents(int monthId)
+        {
+            var events = _eventServices.GetMonthEvents(monthId);
+            if (events != null)
+            {
+                var eventsList = events as List<EventModel> ?? events.ToList();
+                if (eventsList.Any())
+                    return Request.CreateResponse(HttpStatusCode.OK, eventsList);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No Events in this month");
+        }
+
+        // GET api/Events/GetEventDetails?eventId={eventId}
+        public HttpResponseMessage GetEventDetails(int eventId)
+        {
+            var eventDetails = _eventServices.GetEventDetails(eventId);
+            if (eventDetails != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, eventDetails);
+            }
+
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Couldn't find event details");
+        }
+    }
+}
